96730e85810ce06b37a90a986402531b744bcf70
annawojcieszek <annawojcieszek@Mac-mini-Anna.local>
2014-08-30 21:53:07 +0200

    Poprawa mailingu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Sklep::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'wdomunajlepiej.megiteam.pl' }
+  config.action_mailer.default_url_options = { host: 'cukoo.pl' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
